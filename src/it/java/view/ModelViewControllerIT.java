3485c9966f9ad2a10fee6f69a633e160f8f01161
amathebest <amatori.matt@gmail.com>
2020-08-23 17:20:51 +0200

    Wrote mvc integration tests

---
+++ src/it/java/view/ModelViewControllerIT.java
@@ -0,0 +1,81 @@
+package view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.assertj.swing.core.matcher.JButtonMatcher;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.ClassRule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.testcontainers.containers.GenericContainer;
+
+import controller.SchoolController;
+import model.Student;
+import repository.StudentMongoRepository;
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
+@RunWith(GUITestRunner.class)
+public class ModelViewControllerIT extends AssertJSwingJUnitTestCase {
+	@SuppressWarnings("rawtypes")
+	@ClassRule
+	public static final GenericContainer mongo =
+		new GenericContainer("mongo:4.0.5") 
+			.withExposedPorts(27017);
+
+	private MongoClient mongoClient;
+
+	private FrameFixture window;
+	private SchoolController schoolController;
+	private StudentMongoRepository studentRepository;
+
+	@Override
+	protected void onSetUp() {
+		mongoClient = new MongoClient(new ServerAddress(mongo.getContainerIpAddress(), mongo.getMappedPort(27017)));
+		studentRepository = new StudentMongoRepository(mongoClient);
+		// explicit empty the database through the repository
+		for (Student student : studentRepository.findAll()) {
+			studentRepository.delete(student.getId());
+		}
+		window = new FrameFixture(robot(), GuiActionRunner.execute(() -> {
+			StudentSwingView studentSwingView = new StudentSwingView();
+			schoolController = new SchoolController(studentSwingView, studentRepository);
+			studentSwingView.setSchoolController(schoolController);
+			return studentSwingView;
+		}));
+		window.show(); // shows the frame to test
+	}
+
+	@Override
+	protected void onTearDown() {
+		mongoClient.close();
+	}
+
+	@Test
+	public void testAddStudent() {
+		// use the UI to add a student...
+		window.textBox("idTextBox").enterText("1");
+		window.textBox("nameTextBox").enterText("test");
+		window.button(JButtonMatcher.withText("Add")).click();
+		// ...verify that it has been added to the database
+		assertThat(studentRepository.findById("1")).isEqualTo(new Student("1", "test"));
+	}
+
+	@Test
+	public void testDeleteStudent() {
+		// add a student needed for tests
+		studentRepository.save(new Student("99", "existing"));
+		// use the controller's allStudents to make the student
+		// appear in the GUI list
+		GuiActionRunner.execute(() -> schoolController.allStudents());
+		// ...select the existing student
+		window.list().selectItem(0);
+		window.button(JButtonMatcher.withText("Delete Selected")).click();
+		// verify that the student has been deleted from the db
+		assertThat(studentRepository.findById("99")).isNull();
+	}
+
+}
---
+++ src/it/java/view/StudentSwingViewIT.java
@@ -0,0 +1,119 @@
+package view;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.InetSocketAddress;
+
+import org.assertj.swing.annotation.GUITest;
+import org.assertj.swing.core.matcher.JButtonMatcher;
+import org.assertj.swing.edt.GuiActionRunner;
+import org.assertj.swing.fixture.FrameFixture;
+import org.assertj.swing.junit.runner.GUITestRunner;
+import org.assertj.swing.junit.testcase.AssertJSwingJUnitTestCase;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import controller.SchoolController;
+import model.Student;
+import view.StudentSwingView;
+import repository.StudentMongoRepository;
+import com.mongodb.MongoClient;
+import com.mongodb.ServerAddress;
+
+import de.bwaldvogel.mongo.MongoServer;
+import de.bwaldvogel.mongo.backend.memory.MemoryBackend;
+
+
+@RunWith(GUITestRunner.class)
+public class StudentSwingViewIT extends AssertJSwingJUnitTestCase {
+	private static MongoServer server;
+	private static InetSocketAddress serverAddress;
+	private MongoClient mongoClient;
+	
+	private FrameFixture window;
+	private StudentSwingView studentSwingView;
+	private SchoolController schoolController;
+	private StudentMongoRepository studentRepository;
+	
+	@BeforeClass
+	public static void setupServer() {
+		server = new MongoServer(new MemoryBackend());
+		serverAddress = server.bind();
+	}
+	
+	@AfterClass
+	public static void shutdownServer() {
+		server.shutdown();
+	}
+	
+	@Override
+	protected void onSetUp() {
+		mongoClient = new MongoClient(new ServerAddress(serverAddress));
+		studentRepository = new StudentMongoRepository(mongoClient);
+		for (Student student : studentRepository.findAll()) {
+			studentRepository.delete(student.getId());
+		}
+		GuiActionRunner.execute(() -> {
+			studentSwingView = new StudentSwingView();
+			schoolController = new SchoolController(studentSwingView, studentRepository);
+			studentSwingView.setSchoolController(schoolController);
+			return studentSwingView;
+		});
+		window = new FrameFixture(robot(), studentSwingView);
+		window.show();
+	}
+		
+	@Override
+	protected void onTearDown() {
+		mongoClient.close();
+	}
+		
+	@Test
+	public void testAllStudents() {
+		Student student1 = new Student("1", "test1");
+		Student student2 = new Student("2", "test2");
+		studentRepository.save(student1);
+		studentRepository.save(student2);
+		GuiActionRunner.execute(() -> schoolController.allStudents());
+		assertThat(window.list().contents()).containsExactly(student1.toString(), student2.toString());
+	}
+	
+	@Test
+	public void testAddButtonSuccess() {
+		window.textBox("idTextBox").enterText("1");
+		window.textBox("nameTextBox").enterText("test");
+		window.button(JButtonMatcher.withText("Add")).click();
+		assertThat(window.list().contents()).containsExactly(new Student("1", "test").toString());
+	}
+	
+	@Test
+	public void testAddButtonError() {
+		studentRepository.save(new Student("1", "existing"));
+		window.textBox("idTextBox").enterText("1");
+		window.textBox("nameTextBox").enterText("test");
+		window.button(JButtonMatcher.withText("Add")).click();
+		assertThat(window.list().contents()).isEmpty();
+		window.label("errorLabel").requireText("Already existing student with id 1: " + new Student("1", "existing"));
+	}
+	
+	@Test
+	public void testDeleteButtonSuccess() {
+		GuiActionRunner.execute(() -> schoolController.newStudent(new Student("1", "toremove")));
+		window.list().selectItem(0);
+		window.button(JButtonMatcher.withText("Delete Selected")).click();
+		assertThat(window.list().contents()).isEmpty();
+	}
+	
+	@Test
+	public void testDeleteButtonError() {
+		Student student = new Student("1", "test");
+		GuiActionRunner.execute(() -> studentSwingView.getListStudentsModel().addElement(student));
+		window.list().selectItem(0);
+		window.button(JButtonMatcher.withText("Delete Selected")).click();
+		assertThat(window.list().contents()).containsExactly(student.toString());
+		window.label("errorLabel").requireText("No existing student with id 1: " + student);
+	}
+	
+}
